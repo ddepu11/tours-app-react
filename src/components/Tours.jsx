f1a9370d6fa9c4107cf7efb630b38172372eea89
Deepanshu Tiwari <ddepu11@gmail.com>
2021-03-07 16:48:18 +0530

    Added rupee symbol

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import Button from "./Button";
 
 const Tours = ({ image, price, name, info, onClick, id }) => {
+  const [showMore, setShowMore] = useState(false);
+
+  const toggleShow = () => {
+    setShowMore(!showMore);
+  };
+
   return (
     <div className="card">
       <div className="view">
@@ -8,12 +15,14 @@ const Tours = ({ image, price, name, info, onClick, id }) => {
       </div>
       <div className="top flex">
         <h5>{name}</h5>
-        <p>{price}</p>
+        <p>&#x20B9;{price}</p>
       </div>
       <div className="bottom">
         <p>
-          {info}
-          <Button btnStyle="show">Show More</Button>
+          {showMore ? info : `${info.slice(0, 295).trim()}...`}
+          <Button onClick={toggleShow} btnStyle="show">
+            {showMore ? "Show Less" : "Show More"}
+          </Button>
         </p>
       </div>
       <Button
