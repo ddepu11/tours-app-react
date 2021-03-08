ef588023963a7f983a37f1a72fd94abff042a7f7
Deepanshu Tiwari <ddepu11@gmail.com>
2021-03-08 10:42:07 +0530

    Refresh Button added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Tour from "./components/Tours";
 import tours from "./tours";
+import Button from "./components/Button";
 
 export default function App() {
   const [toursArr, setToursArr] = useState([]);
@@ -10,25 +11,34 @@ export default function App() {
 
   function onClick(e) {
     const { id } = e.target;
-
     setToursArr(toursArr.filter((item) => item.id !== id));
   }
-
+  function reFresh() {
+    setToursArr(tours);
+  }
   return (
     <div className="container">
-      <h1 className="heading">Our Different Tours </h1>
+      <h1 className="heading">
+        {toursArr.length !== 0 ? "Our Different Tours" : "No Tours Left"}
+      </h1>
       <div className="line"></div>
-      {toursArr.map(({ name, image, info, price, id }, index) => (
-        <Tour
-          onClick={onClick}
-          id={id}
-          name={name}
-          info={info}
-          image={image}
-          price={price}
-          key={index}
-        />
-      ))}
+      {toursArr.length !== 0 ? (
+        toursArr.map(({ name, image, info, price, id }, index) => (
+          <Tour
+            onClick={onClick}
+            id={id}
+            name={name}
+            info={info}
+            image={image}
+            price={price}
+            key={index}
+          />
+        ))
+      ) : (
+        <Button onClick={reFresh} btnStyle="refersh-btn">
+          Refersh
+        </Button>
+      )}
     </div>
   );
 }
